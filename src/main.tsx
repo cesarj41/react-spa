bcd3382b6ed3d10ca4ad264b3f5bdebb00c16ea8
Cesar Martinez <cesarj4114@gmail.com>
2023-05-17 02:30:08 -0400

    chore(setup): configured react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Router } from "./router";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./main.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   </React.StrictMode>
 );
