9657e41b6362d579229f341b8cf140fc3a8c5847
Cesar Martinez <cesarj4114@gmail.com>
2023-06-14 10:04:56 -0400

    chore: update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Router } from "./router";
-import { QueryClient, QueryClientProvider } from "react-query";
 import "./main.css";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router />
-    </QueryClientProvider>
+    <Router />
   </React.StrictMode>
 );
